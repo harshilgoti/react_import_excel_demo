8841aa521d11e455fd9a28dc2ede248e487049e6
harshil goti <harshilgoti@gmail.com>
2021-04-29 14:57:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import readXlsxFile from "read-excel-file";
 
 function App() {
+  const handleChange = (e) => {
+    readXlsxFile(e.target.files[0])
+      .then((rows) => {
+        console.log("rows", rows);
+        const update = rows.slice(1).map((item) => {
+          return item.reduce((acc, cur, i) => {
+            acc = Object.assign(acc, { [rows[0][i]]: cur });
+            return acc;
+          }, {});
+        });
+        console.log("update", update);
+      })
+      .catch((error) => console.log("error", error));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Open console and see output</h3>
+      <input type="file" id="input" onChange={handleChange} />
+      <h4>Athor : HARSHIL GOTI </h4>
     </div>
   );
 }
